8e5db8c59b17f86c74cd05ef6a4036ff596656ae
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-11-26 11:31:18 +0200

    Added Bloglist componenet

---
+++ src/assets/BlogList.jsx
@@ -0,0 +1,14 @@
+function BlogList({ posts }) {
+    return (
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  
+  export default BlogList;
