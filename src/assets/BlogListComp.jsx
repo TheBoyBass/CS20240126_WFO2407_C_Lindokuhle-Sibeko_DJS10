9079f4675f9e4d53164d17d622f4dec8c5bfae06
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-11-26 12:06:12 +0200

    "Refactor CSS and JSX files: remove logo styling, update typography and layout, and modify BlogList component"

--- src/assets/BlogListComp.jsx
+++ src/assets/BlogListComp.jsx
@@ -1,14 +1,14 @@
 function BlogList({ posts }) {
-    return (
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-  
-  export default BlogList;
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <h3 className="post-heading">{post.title}</h3>
+          <p className="post-paragraph">{post.body}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default BlogList;
